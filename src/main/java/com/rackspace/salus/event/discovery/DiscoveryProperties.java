52d86744175ec6e7c582c57d94ad50872144a92f
Geoff Bourne <itzgeoff@gmail.com>
2019-06-10 13:41:17 -0500

    Update src/main/java/com/rackspace/salus/event/discovery/DiscoveryProperties.java
    Co-Authored-By: James Buchan <jamesjbuchan@googlemail.com>

--- src/main/java/com/rackspace/salus/event/discovery/DiscoveryProperties.java
+++ src/main/java/com/rackspace/salus/event/discovery/DiscoveryProperties.java
@@ -43,7 +43,7 @@ public class DiscoveryProperties {
   public static class KubernetesStrategy {
 
     /**
-     * Specifies the endpoint where the Kubernetes API can be access. The default is the typical
+     * Specifies the endpoint where the Kubernetes API can be accessed. The default is the typical
      * URL that works for pods deployed within the cluster to be queried. The service account
      * that accesses the API will need to be given get, list, and watch verb access to the resources
      * endpoints and services.
